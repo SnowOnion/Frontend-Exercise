3dbe416ca3990aa9915768dcde7c8a3d0de06394
SnowOnion <SnowOnionLee@gmail.com>
2014-12-02 21:20:15 +0800

    1

--- js/global.js
+++ js/global.js
@@ -1,2 +1,19 @@
 ;
-console.log('GLOBAL START')
+console.log('-GLOBAL START');
+
+var x = 2;
+function Test(){
+    this.x = 1;
+}
+var o = new Test();
+console.log("o.x: " + o.x); //1
+o.x = 3;
+window.Test(); // 等于 Test()
+//new Test();
+x*=10;
+console.log("o.x: " + o.x); //3
+console.log("x: " + x); //10
+
+console.log('GLOBAL END---');
+
+
