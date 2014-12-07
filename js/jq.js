976f27772aeb118dc140574f74f0cbf5a401568c
SnowOnion <SnowOnionLee@gmail.com>
2014-12-07 16:29:41 +0800

    ok intro to three.js

---
+++ js/jq.js
@@ -0,0 +1,18 @@
+;
+//$(document).ready(alert('document ready'));
+
+var LOG_AREA=$(".console-div textarea");
+//var LOG_AREA=$("#console-area")
+
+function lg(msg){
+
+}
+
+function a(){
+
+}
+
+function b(){
+    alert(LOG_AREA.val());
+    console.log(LOG_AREA);
+}
